8ea175609a18118bc2c47257da95d8a8e9840cc0
Martae Marshall <martaemarshall@gmail.com>
2023-08-04 17:51:41 -0500

    Styled Header Component based on guide

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,20 @@
 import logo from "../assets/Logo.svg";
+import style from "./Header.module.css";
 
 function Header() {
     return (
-        <header>
+        <header id={style.header}>
             <div>
                 <img src={logo} alt='A drawing of a lemon next to the words Little Lemon'/>
             </div>
-            <nav>
+            <nav className={style.nav}>
                 <ul>
-                    <li a='/'>HOME</li>
-                    <li a='/'>ABOUT</li>
-                    <li a='/'>MENU</li>
-                    <li a='/'>RESERVATIONS</li>
-                    <li a='/'>ORDER ONLINE</li>
-                    <li a='/'>LOGIN</li>
+                    <li a='#'>HOME</li>
+                    <li a='#'>ABOUT</li>
+                    <li a='#'>MENU</li>
+                    <li a='#'>RESERVATIONS</li>
+                    <li a='#'>ORDER ONLINE</li>
+                    <li a='#'>LOGIN</li>
                 </ul>
             </nav>
         </header>
