678b4c05a238905cfaa75aa1f056457d0d40500c
Martae Marshall <martaemarshall@gmail.com>
2023-08-05 12:50:03 -0500

    Styled Highlights section for Little Lemon

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -1,21 +1,25 @@
-//import salad from "../assets/greek salad.jpg";
-//import bruchetta from "../assets/bruchetta.svg";
-//import dessert from "../assets/lemon dessert.jpg";
-//include code for cards
+import salad from "../assets/greek_salad.jpg";
+import bruchetta from "../assets/bruchetta.jpg";
+import dessert from "../assets/lemon_dessert.jpg";
+import Card from "./Card";
+import style from "./Card.css";
+import style2 from "./Highlights.module.css";
 
 function Highlights() {
     return (
         <section>
             <div>
-                <h2>Specials</h2>
-                <div>
-                    <button>Online Menu</button>
+                <div className={style2.topline}>
+                    <h2>Specials</h2>
+                    <div className={style2.menubutton}>
+                        <button>Online Menu</button>
+                    </div>
                 </div>
-                <div>
-                    <p>Cards for highlights</p>
+                <div className="menu">
+                    <Card imageUrl={salad} imageAlt="Greek Salad" item="Greek Salad" price="12.99" description="The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons." />
+                    <Card imageUrl={bruchetta} imageAlt="Bruchetta" item="Bruchetta" price="5.99" description="Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil." />
+                    <Card imageUrl={dessert} imageAlt="Lemon Dessert" item="Lemon Dessert" price="5.00" description="This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined." />
                 </div>
-            </div>
-            <div>
             </div>
         </section>
 
