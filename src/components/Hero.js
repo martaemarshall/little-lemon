bf59a93f3ec49be06d15720470d069839df42d3c
Martae Marshall <martaemarshall@gmail.com>
2023-08-04 20:29:49 -0500

    Stylized the Hero section of Little Lemon

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,19 @@
 import food from "../assets/restauranfood.jpg";
+import style from "./Hero.module.css";
 
 function Hero() {
     return (
-        <section>
-            <div>
-                <h1>Little Lemon</h1>
-                <h3>Chicago</h3>
-                <p>A brief description of what type of food it offers</p>
+        <section id={style.hero}>
+            <div className={style.container}>
+                <h1 className={style.title}>Little Lemon</h1>
+                <h2 className={style.title}>Chicago</h2>
+                <p>We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
                 <div>
-                    <button>Reserve a Table</button>
+                    <button className={style.buttonreserve}>Reserve a Table</button>
                 </div>
             </div>
-            <div>.
-                <img src={food} alt="A Mediterranean chicken dish made at Little Lemon" width="200px" height="300px" />
+            <div className={style.container}>
+                <img src={food} alt="A Mediterranean chicken dish made at Little Lemon" width="300px" height="350px" />
             </div>
         </section>
     )
